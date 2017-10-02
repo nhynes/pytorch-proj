e25e5cf29e44a28264c6b564c11a7b28b020ee36
Nick Hynes <nhynes@berkeley.edu>
2017-10-02 06:47:36 -0700

    Add Vocab ADT

--- common.py
+++ common.py
@@ -110,3 +110,52 @@ def load_txt(path_txt):
     """Loads a text file."""
     with open(path_txt) as f_txt:
         return [line.rstrip() for line in f_txt]
+
+
+class Vocab(object):
+    """Represents a token2index and index2token map."""
+
+    def __init__(self, tok_counts, unk_tok=None):
+        """Constructs a Vocab ADT."""
+        self.tok_counts = tok_counts
+        self.w2i = {w: i for i, (w, _) in enumerate(self.tok_counts)}
+
+        self.unk_tok = unk_tok
+        if unk_tok is not None:
+            assert unk_tok in self.w2i
+            self.unk_idx = self.w2i[unk_tok]
+
+    def __getitem__(self, index):
+        if isinstance(index, int):
+            if index < len(self):
+                return self.tok_counts[index][0]
+            elif self.unk_tok:
+                return self.unk_idx
+            else:
+                raise IndexError(f'No token in position {index}!')
+        elif isinstance(index, str):
+            if index in self.w2i:
+                return self.w2i[index]
+            elif self.unk_tok:
+                return self.unk_idx
+            else:
+                raise KeyError(f'{index} not in vocab!')
+        else:
+            raise ValueError('Index to Vocab must be string or int.')
+
+    def add_extra_vocab(self, extra_vocab):
+        """Returns a new Vocab with extra tokens prepended."""
+        extra_tok_counts = [(w, float('inf')) for w in extra_vocab]
+        return Vocab(extra_tok_counts + self.tok_counts,
+                     unk_tok=self.unk_tok)
+
+    def set_unk_tok(self, unk_tok):
+        """Sets the token/index to return when looking up an OOV token."""
+        return Vocab(self.tok_counts, unk_tok=unk_tok)
+
+    def truncate(self, size):
+        """Returns a new Vocab containing the top `size` tokens."""
+        return Vocab(self.tok_counts[:size], unk_tok=self.unk_tok)
+
+    def __len__(self):
+        return len(self.tok_counts)
